65ed3728935575c831a7495c0ddc49d5dc0dce9c
Bharat Reddy <reddy.bharat.11@gmail.com>
2020-04-02 21:40:58 +0530

    init repo

---
+++ image_process.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import os
+
+# define path for the images to be read and where to be saved
+old_path = "/images/"
+new_path = "/images/"
+
+for fh in os.listdir(old_path):
+    img = Image.open(old_path + fh)
+    img = img.rotate(-90)
+    img = img.resize((128, 128))
+    img = img.convert("RGB")
+    img.save(new_path + fh, "JPEG")
---
+++ send_email.py
@@ -0,0 +1,56 @@
+# Script to email an attachment and list of people
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+import os
+
+
+def generate(sender, receiver, subject, body):
+    # email formatting
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = receiver
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+
+    # #process and add attachment to email
+    # filename = os.path.basename(path)
+    # mime_type, _ = mimetypes.guess_type(path)
+    # mime_type, mime_subtype = mime_type.split('/', 1)
+
+    # with open(path, 'rb') as file:
+    # 	msg.add_attachment(file.read(),
+    # 		maintype = mime_type,
+    # 		subtype = mime_subtype,
+    # 		filename = filename)
+
+    return msg
+
+
+def send(msg):
+    session = smtplib.SMTP("smtp.gmail.com", 587)
+    session.starttls()
+    session.login(sender, password)
+    session.send_message(msg)
+    session.quit()
+    print("\nMail successfully sent.")
+
+
+# <-------------------------------------------------->
+
+
+# create message here
+subject = "TEST SCRIPT"
+body = "Hello, this is a test mail."
+
+# sender and receiver details
+sender = "xxhelloxx101@gmail.com"
+password = "print(hello)"
+receiver = "reddy.bharat.11@gmail.com"
+
+
+if __name__ == "__main__":
+    mail = generate(sender, receiver, subject, body)
+    send(mail)
+
